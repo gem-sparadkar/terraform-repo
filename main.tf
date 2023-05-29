a44bb16302c546ec84ce8d8f96cba6f49045526f
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-05-29 14:09:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 # provider "aws:3.7.1" {
 #   region = "ap-south-1"
 # }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+provider "aws" {
+  access_key = jsondecode(file("aws_credentials.json"))["accessKeyId"]
+  secret_key = jsondecode(file("aws_credentials.json"))["secretAccessKey"]
+  region     = ap-south-1
 }
+
 resource "aws_s3_bucket" "test" {
   bucket = "my-test-s3-terraform-bucket"
   acl = "private"
