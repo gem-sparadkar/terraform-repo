3614963f6ea2a2379977d2924011e2d531013c08
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-05-29 13:25:50 +0530

    Update main.tf added provider version

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
-provider "aws" {
-  region = "ap-south-1"
+# provider "aws:3.7.1" {
+#   region = "ap-south-1"
+# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.7.1"
+    }
+  }
 }
-
 resource "aws_s3_bucket" "test" {
   bucket = "my-test-s3-terraform-bucket"
   acl = "private"
