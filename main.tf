16f4cc032e6dcb59a60bb402a30402497521c3b0
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-05-29 14:32:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 #   region = "ap-south-1"
 # }
 provider "aws" {
-  access_key = jsondecode(file("aws_credentials.json"))["accessKeyId"]
-  secret_key = jsondecode(file("aws_credentials.json"))["secretAccessKey"]
+  access_key = jsondecode(file("aws_credentials.json"))["Access_Key"]
+  secret_key = jsondecode(file("aws_credentials.json"))["Secret_Access_Key"]
   region     = jsondecode(file("aws_credentials.json"))["Region"]
 }
 
