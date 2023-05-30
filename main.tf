6670da7ad0470085a16d7bb3bc2ddf69102ae36c
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-05-30 17:34:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@
 provider "aws" {
   region     = "ap-south-1"
 }
-terraform { 
-required_version= ">=0.13" 
-backend "s3" { 
-  region = "ap-south-1" 
-  dynamodb_table = "lockid-table" 
-  key = "terraform.tfstate" 
-  bucket = "tfstatefile-store" 
-  skip_credentials_validation = true
-  } 
+# terraform { 
+# required_version= ">=0.13" 
+# backend "s3" { 
+#   region = "ap-south-1" 
+#   dynamodb_table = "lockid-table" 
+#   key = "terraform.tfstate" 
+#   bucket = "tfstatefile-store" 
+#   skip_credentials_validation = true
+#   } 
 
-} 
+# } 
 
 resource "aws_s3_bucket" "test" {
   bucket = "my-test-s3-terraform-bucket-new"
