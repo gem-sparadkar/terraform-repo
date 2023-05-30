b5ac2253072c7c077fbfe6a46f4e3ab78d0d4a57
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-05-30 17:50:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 # provider "aws:3.7.1" {
 #   region = "ap-south-1"
 # }
+terraform {
+  # Can be removed when bug is resolved: https://github.com/hashicorp/terraform-provider-aws/issues/23110
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "aws" {
   region     = "ap-south-1"
 }
