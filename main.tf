318ca7129fef42d9b7198613f77cc31712b26076
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-05-30 13:55:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@
 #   region = "ap-south-1"
 # }
 provider "aws" {
-#   access_key = jsondecode(file("aws_credentials.json"))["Access_Key"]
-#   secret_key = jsondecode(file("aws_credentials.json"))["Secret_Access_Key"]
-#   region     = jsondecode(file("aws_credentials.json"))["Region"]
+  access_key = jsondecode(file("aws_credentials.json"))["Access_Key"]
+  secret_key = jsondecode(file("aws_credentials.json"))["Secret_Access_Key"]
+  region     = jsondecode(file("aws_credentials.json"))["Region"]
 }
 terraform { 
 required_version= ">=0.13" 
