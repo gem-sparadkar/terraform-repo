1c70d0f1055567a6280c5f71c33a4a9f2a4fe210
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-06-01 13:21:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.7.1"
+      version = ">=0.13"
     }
 #   backend "s3" { 
 #     region = "ap-south-1" 
