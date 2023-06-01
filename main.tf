a222e7e78385d9c1027534e0a3536be212a130a8
gem-sparadkar <112152338+gem-sparadkar@users.noreply.github.com>
2023-06-01 11:49:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,8 @@
 # }
 provider "aws" {
   version = "3.57.0"
-  profile="798092881332"
+  shared_credentials_files = ["C:\Users\sakshi.paradkar\.aws\credentials"]
+  profile                  = "default"
   assume_role {
     role_arn     = "arn:aws:sts::798092881332:assumed-role/codebuild-terraform_build-service-role/AWSCodeBuild-1605a7bf-c7a0-4d13-8b8c-7f7ff7c2ce5c"
     session_name= "terraform"
